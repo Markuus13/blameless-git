aa3274944e36cb9b5920e27e8cbb99a760988f2e
Marcus Vinícius da Silva Borges <marcus.f22@gmail.com>
2021-08-06 20:49:06 -0300

    feat: creates extraction attributes from git url

---
+++ lib/domain/services/extract_attributes_from_git_url.rb
@@ -0,0 +1,27 @@
+require 'dry/monads'
+
+require_relative '../constants'
+
+class ExtractAttributesFromGitUrl
+  include Dry::Monads[:result]
+
+  def call(repository_url)
+    url_matches = repository_url.match(Constants::PROVIDERS_URL_FORMAT)
+    git_repository_attributes = split_git_repository_data(url_matches)
+    return Failure('URL does not match a known git repository') if git_repository_attributes.empty?
+
+    Success(git_repository_attributes.merge(original_url: repository_url))
+  end
+
+  private
+
+  def split_git_repository_data(url_matches)
+    return {} if url_matches.nil?
+
+    {
+      provider_name: url_matches[1],
+      owner_name: url_matches[2],
+      name: url_matches[3],
+    }
+  end
+end
---
+++ spec/domain/services/extract_attributes_from_git_url_spec.rb
@@ -0,0 +1,38 @@
+require_relative '../../../lib/domain/services/extract_attributes_from_git_url'
+
+RSpec.describe ExtractAttributesFromGitUrl do
+  subject(:extract_attributes_from_git_url) { described_class.new }
+
+  let(:git_repository_url) { 'https://github.com/Markuus13/CleanFoodJava' }
+
+  it 'succeeds' do
+    result = extract_attributes_from_git_url.call(git_repository_url)
+    expect(result.success?).to be(true)
+  end
+
+  it 'returns a hash with extracted data' do
+    result = extract_attributes_from_git_url.call(git_repository_url)
+    expect(result.value!).to eq(
+      original_url: git_repository_url,
+      provider_name: 'github',
+      owner_name: 'Markuus13',
+      name: 'CleanFoodJava',
+    )
+  end
+
+  describe 'validations' do
+    context 'when url is not a valid git repository' do
+      let(:invalid_git_repository_url) { 'https://google.com.br' }
+
+      it 'fails' do
+        result = extract_attributes_from_git_url.call(invalid_git_repository_url)
+        expect(result.failure?).to be(true)
+      end
+
+      it 'returns a error message' do
+        result = extract_attributes_from_git_url.call(invalid_git_repository_url)
+        expect(result.failure).to eq('URL does not match a known git repository')
+      end
+    end
+  end
+end
