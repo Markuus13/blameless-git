5c79d42921d80a8884738743e4529b618271babc
Marcus Vinícius da Silva Borges <marcus.f22@gmail.com>
2021-08-10 17:51:37 -0300

    test: adds specs related to git repository creation

--- spec/database/mock_git_repo_repository_spec.rb
+++ spec/database/mock_git_repo_repository_spec.rb
@@ -10,4 +10,114 @@ RSpec.describe MockGitRepoRepository do
     it { is_expected.to respond_to(:find).with(1).argument }
     it { is_expected.to respond_to(:save).with(1).argument }
   end
+
+  describe '#all' do
+    after { mock_git_repo_repository.destroy_all }
+
+    context 'when no git repositories were saved' do
+      it 'returns an empty array' do
+        expect(mock_git_repo_repository.all).to eq([])
+      end
+    end
+
+    context 'when exists saved git repositories' do
+      it 'returns all git repositories' do
+        hanami_repository = GitRepository.new(original_url: 'https://github.com/hanami/hanami')
+        grape_repository = GitRepository.new(original_url: 'https://github.com/ruby-grape/grape')
+        mock_git_repo_repository.save(hanami_repository)
+        mock_git_repo_repository.save(grape_repository)
+
+        expect(mock_git_repo_repository.all).to match_array([hanami_repository, grape_repository])
+      end
+    end
+  end
+
+  describe '#find' do
+    after { mock_git_repo_repository.destroy_all }
+
+    context 'when id is not from a saved git repository' do
+      it 'returns nil' do
+        result = mock_git_repo_repository.find(1)
+
+        expect(result).to eq(nil)
+      end
+    end
+
+    context 'when id is from a saved git repository' do
+      it 'returns a git repository' do
+        hanami_repository = GitRepository.new(original_url: 'https://github.com/hanami/hanami')
+        git_repository = mock_git_repo_repository.save(hanami_repository)
+
+        result = mock_git_repo_repository.find(git_repository.id)
+
+        expect(result).to eq(git_repository)
+      end
+    end
+  end
+
+  describe '#save' do
+    after { mock_git_repo_repository.destroy_all }
+
+    context 'when a git repository with given url does not exist' do
+      it 'creates a new one' do
+        hanami_repository = GitRepository.new(original_url: 'https://github.com/hanami/hanami')
+
+        expect { mock_git_repo_repository.save(hanami_repository) }
+          .to change { mock_git_repo_repository.all.size }.from(0).to(1)
+      end
+
+      it 'returns the created git repository with an id' do
+        hanami_repository = GitRepository.new(original_url: 'https://github.com/hanami/hanami')
+
+        result = mock_git_repo_repository.save(hanami_repository)
+
+        expect(result).to be_a(GitRepository)
+        expect(result.id).to_not be(nil)
+      end
+    end
+
+    context 'when id is from a saved git repository' do
+      it 'does not create a new one' do
+        git_repository = GitRepository.new(original_url: 'https://github.com/hanami/lotus')
+        mock_git_repo_repository.save(git_repository)
+        git_repository.name = 'hanami'
+
+        expect { mock_git_repo_repository.save(git_repository) }
+          .to_not(change { mock_git_repo_repository.all.size })
+      end
+
+      it 'updates the saved git repository with new values' do
+        git_repository = GitRepository.new(original_url: 'https://github.com/lotus/lotus')
+        mock_git_repo_repository.save(git_repository)
+        git_repository.name = 'hanami'
+
+        result = mock_git_repo_repository.save(git_repository)
+
+        expect(result.name).to eq(git_repository.name)
+      end
+    end
+  end
+
+  describe '#destroy_all' do
+    after { mock_git_repo_repository.destroy_all }
+
+    context 'when no git repositories were saved' do
+      it 'changes nothing' do
+        expect { mock_git_repo_repository.destroy_all }
+          .to_not(change { mock_git_repo_repository.all.size })
+      end
+    end
+
+    context 'when exists saved git repositories' do
+      it 'deletes all saved git repositories' do
+        hanami_repository = GitRepository.new(original_url: 'https://github.com/hanami/hanami')
+        grape_repository = GitRepository.new(original_url: 'https://github.com/ruby-grape/grape')
+        mock_git_repo_repository.save(hanami_repository)
+        mock_git_repo_repository.save(grape_repository)
+
+        expect { mock_git_repo_repository.destroy_all }
+          .to change { mock_git_repo_repository.all.size }.from(2).to(0)
+      end
+    end
+  end
 end
--- spec/domain/use_cases/create_obfuscated_git_repository_spec.rb
+++ spec/domain/use_cases/create_obfuscated_git_repository_spec.rb
@@ -5,13 +5,13 @@ require_relative '../../../lib/domain/use_cases/create_obfuscated_git_repository
 RSpec.describe CreateObfuscatedGitRepository do
   let(:in_memory_git_repo_repository) { MockGitRepoRepository.new }
 
-  before { in_memory_git_repo_repository.destroy_all }
+  after { in_memory_git_repo_repository.destroy_all }
 
   subject(:create_obfuscated_git_repository) do
     described_class.new(git_repo_repository: in_memory_git_repo_repository)
   end
 
-  context 'when url is accepted based on our providers' do
+  context 'when given url is a valid git repository url' do
     let(:git_repository_url) { 'https://github.com/sirius-black/marauders-map' }
 
     it 'succeeds' do
@@ -19,7 +19,7 @@ RSpec.describe CreateObfuscatedGitRepository do
       expect(result.success?).to eq(true)
     end
 
-    it 'returns an created obfuscated git repository' do
+    it 'returns a hash of created obfuscated git repository' do
       result = create_obfuscated_git_repository.call(git_repository_url)
       git_repository = in_memory_git_repo_repository.all.last
       expect(result.value!).to eq(
@@ -30,6 +30,20 @@ RSpec.describe CreateObfuscatedGitRepository do
         owner_name: git_repository.owner_name,
         provider_name: git_repository.provider_name
       )
+    end
+  end
+
+  context 'when given url is not a valid git repository url' do
+    let(:not_a_git_repository_url) { 'https://github.com' }
+
+    it 'fails' do
+      result = create_obfuscated_git_repository.call(not_a_git_repository_url)
+      expect(result.failure?).to eq(true)
+    end
+
+    it 'returns an error message' do
+      result = create_obfuscated_git_repository.call(not_a_git_repository_url)
+      expect(result.failure).to be_a(String)
     end
   end
 end
